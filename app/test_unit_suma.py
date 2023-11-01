6328611c150c83b7c6150da094e5b0df8d529af3
AricardoViVe <128760591+AricardoViVe@users.noreply.github.com>
2023-11-01 01:32:32 -0600

    Coreecciones de estilo con black

--- app/test_unit_suma.py
+++ app/test_unit_suma.py
@@ -1,39 +1,79 @@
+import sys
 
-import sys 
-sys.path.append('/Users/rich/Documents/DiploMLops/20102023/practica_uno/Integra/practicaS5/app')  
+sys.path.append(
+    "/Users/rich/Documents/DiploMLops/20102023/practica_uno/Integra/practicaS5/app"
+)
 
 from calculadora import Calculadora
 import pytest
+
 calc = Calculadora()
 
-#Prueba Suma
+
+# Prueba Suma
 def obtener_datos_suma():
-    return [(1,1,2.0),('1','2',3.0),(2.0,'1',3.0)]
-@pytest.mark.parametrize('a','b','esperado',obtener_datos_suma())
-def test_suma_parametrize(a,b,esperado):
-    assert calc.suma(a,b) == esperado
+    return [(1, 1, 2.0), ("1", "2", 3.0), (2.0, "1", 3.0)]
+
+
+@pytest.mark.parametrize("a", "b", "esperado", obtener_datos_suma())
+def test_suma_parametrize(a, b, esperado):
+    assert calc.suma(a, b) == esperado
+
+
 from app.calculadora import Calculadora
 import pytest
-calc = Calculadora()
-calc.suma(1+1)
 
-#Prueba Resta
+calc = Calculadora()
+calc.suma(1 + 1)
+
+
+# Prueba Resta
 def obtener_datos_test_resta():
-    return[(2,1,1.0), (2,1.0,1.0), ("2", 1.0,1.0), (2, "1.0",1.0), (2.0, 1.0,1.0), ('2-1',None ,1.0)]
-@pytest.mark.parametrize('a, b, esperado', obtener_datos_test_resta)
-def test_resta_parametrize(a, b,esperado):
+    return [
+        (2, 1, 1.0),
+        (2, 1.0, 1.0),
+        ("2", 1.0, 1.0),
+        (2, "1.0", 1.0),
+        (2.0, 1.0, 1.0),
+        ("2-1", None, 1.0),
+    ]
+
+
+@pytest.mark.parametrize("a, b, esperado", obtener_datos_test_resta)
+def test_resta_parametrize(a, b, esperado):
     assert calc.resta(a, b) == esperado
 
-#Prueba Multiplicación
+
+# Prueba Multiplicación
 def obtener_datos_test_multiplicacion():
-    return[(1,2,2.0), (1,2.0,2.0), ("1", 2.0,2.0), (1, "2.0",2.0), (2.0, 1.0,2.0), ('2*1',None ,2.0)]
-@pytest.mark.parametrize('a, b, esperado', obtener_datos_test_multiplicacion)
-def test_multi_parametrize(a, b,esperado):
+    return [
+        (1, 2, 2.0),
+        (1, 2.0, 2.0),
+        ("1", 2.0, 2.0),
+        (1, "2.0", 2.0),
+        (2.0, 1.0, 2.0),
+        ("2*1", None, 2.0),
+    ]
+
+
+@pytest.mark.parametrize("a, b, esperado", obtener_datos_test_multiplicacion)
+def test_multi_parametrize(a, b, esperado):
     assert calc.multiplicacion(a, b) == esperado
 
-#Prueba División
+
+# Prueba División
 def obtener_datos_test_division():
-    return[(2,1,2.0), (2,1.0,2.0), ("2", 1.0,2.0), (2, "1.0",2.0), (2.0, 1.0,2.0), ('2/1',None ,2.0), (2,0,ValueError)]
-@pytest.mark.parametrize('a, b, esperado', obtener_datos_test_division)
-def test_division_parametrize(a, b,esperado):
+    return [
+        (2, 1, 2.0),
+        (2, 1.0, 2.0),
+        ("2", 1.0, 2.0),
+        (2, "1.0", 2.0),
+        (2.0, 1.0, 2.0),
+        ("2/1", None, 2.0),
+        (2, 0, ValueError),
+    ]
+
+
+@pytest.mark.parametrize("a, b, esperado", obtener_datos_test_division)
+def test_division_parametrize(a, b, esperado):
     assert calc.division(a, b) == esperado
