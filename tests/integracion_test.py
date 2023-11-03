fbeaee7b07f034b1daeb19a4d50fdaf022880ed5
Andrew S <aams_fbm23@ciencias.unam.mx>
2023-11-03 17:50:59 -0600

    correción de formato

--- tests/integracion_test.py
+++ tests/integracion_test.py
@@ -6,7 +6,8 @@ sys.path.append("/Users/rich/Documents/DiploMLops/20102023/practica_uno/Integra/
 
 
 def datos_integracion():
-    return [("(5+5)*(1.25-0.75)*(0.5)^(0.25)", 4.20448207626857), ("(8+7/5)*(15-3/8)*(8/2)^(1/2)", 274.95)]
+    return [("(5+5)*(1.25-0.75)*(0.5)^(0.25)", 4.20448207626857),
+            ("(8+7/5)*(15-3/8)*(8/2)^(1/2)", 274.95)]
 
 
 @pytest.mark.parametrize("operacion, esperado", datos_integracion())
@@ -18,7 +19,8 @@ def test_operacion_parametrize(operacion, esperado):
 
 # Pruebas con funciones anidadas
 def obtener_datos_test_calc_anidada():
-    return [(5, 5, 1.25, -0.75, 0.5, 0.25, 4.20448207626857), (8, 7 / 5, 15, -3 / 8, 8 / 2, 1 / 2, 274.95)]
+    return [(5, 5, 1.25, -0.75, 0.5, 0.25, 4.20448207626857),
+            (8, 7 / 5, 15, -3 / 8, 8 / 2, 1 / 2, 274.95)]
 
 
 @pytest.mark.parametrize("a, b, c, d, e, f, esperado", obtener_datos_test_calc_anidada())  # noqa: E501
