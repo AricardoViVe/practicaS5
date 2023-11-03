86676051752de18e332db8ee5b18affa11cc8d49
Jakelin Rivera <jaky47@gmail.com>
2023-11-02 22:58:26 -0600

    prueba unitaria de potencia y modificacion de prueba integradora

--- tests/integracion_test.py
+++ tests/integracion_test.py
@@ -6,7 +6,7 @@ sys.path.append("/Users/rich/Documents/DiploMLops/20102023/practica_uno/Integra/
 
 
 def datos_integracion():
-    return [("(5+5)*(1.25-0.75)", 5), ("(8+7/5)*(15-3/8)", 137.475)]
+    return [("(5+5)*(1.25-0.75)*(0.5)^(0.25)", 4.20448207626857), ("(8+7/5)*(15-3/8)*(8/2)^(1/2)", 274.95)]
 
 
 @pytest.mark.parametrize("operacion, esperado", datos_integracion())
@@ -18,11 +18,11 @@ def test_operacion_parametrize(operacion, esperado):
 
 # Pruebas con funciones anidadas
 def obtener_datos_test_calc_anidada():
-    return [(5, 5, 1.25, -0.75, 5), (8, 7 / 5, 15, -3 / 8, 137.475)]
+    return [(5, 5, 1.25, -0.75,0.5, 0.25, 4.20448207626857), (8, 7 / 5, 15, -3 / 8, 8 / 2, 1 / 2, 274.95)]
 
 
-@pytest.mark.parametrize("a, b, c, d, esperado", obtener_datos_test_calc_anidada())  # noqa: E501
-def test_cal_anidada(a, b, c, d, esperado):
+@pytest.mark.parametrize("a, b, c, d, e, f, esperado", obtener_datos_test_calc_anidada())  # noqa: E501
+def test_cal_anidada(a, b, c, d,e, f, esperado):
     calc = Calculadora()
-    resultado = calc.multiplicacion(calc.suma(a, b), calc.suma(c, d))
+    resultado = calc.multiplicacion(calc.suma(a, b), calc.suma(c, d), calc.potencia(e, f))
     assert resultado == esperado
