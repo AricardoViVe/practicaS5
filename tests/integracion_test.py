af561f0fa3e438df880bb3a1096ad93e47f979e1
Andrew S <aams_fbm23@ciencias.unam.mx>
2023-11-03 18:09:59 -0600

    actualización de prueba integradora

--- tests/integracion_test.py
+++ tests/integracion_test.py
@@ -6,8 +6,8 @@ sys.path.append("/Users/rich/Documents/DiploMLops/20102023/practica_uno/Integra/
 
 
 def datos_integracion():
-    return [("(5+5)*(1.25-0.75)*(0.5)^(0.25)", 4.20448207626857),
-            ("(8+7/5)*(15-3/8)*(8/2)^(1/2)", 274.95)]
+    return [("((5+5)*(1.25-0.75))*(0.5^0.25)", 4.204482076268572),
+            ("((8+7/5)*(15-3/8))*(8/2^1/2)", 274.95)]
 
 
 @pytest.mark.parametrize("operacion, esperado", datos_integracion())
@@ -19,7 +19,7 @@ def test_operacion_parametrize(operacion, esperado):
 
 # Pruebas con funciones anidadas
 def obtener_datos_test_calc_anidada():
-    return [(5, 5, 1.25, -0.75, 0.5, 0.25, 4.20448207626857),
+    return [(5, 5, 1.25, -0.75, 0.5, 0.25, 4.204482076268572),
             (8, 7 / 5, 15, -3 / 8, 8 / 2, 1 / 2, 274.95)]
 
 
@@ -27,5 +27,5 @@ def obtener_datos_test_calc_anidada():
 def test_cal_anidada(a, b, c, d, e, f, esperado):
     calc = Calculadora()
     resultado = calc.multiplicacion(
-        calc.suma(a, b), calc.suma(c, d), calc.potencia(e, f))
+        calc.suma(a, b), calc.multiplicacion(calc.suma(c, d), calc.potencia_raiz(e, f)))
     assert resultado == esperado
